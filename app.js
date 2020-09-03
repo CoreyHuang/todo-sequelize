51927ef1fd2b617a1e5e330048df5fddc178cef4
Corey <simon8888xsimon8888x@gmail.com>
2020-09-03 23:31:07 +0800

    feat: setting sequelize

--- app.js
+++ app.js
@@ -15,7 +15,27 @@ app.use(bodyParser.urlencoded({ extended: true }))
 const PORT = 3000
 
 app.get('/', (req, res) => {
-  res.send('good')
+  res.send('hello world')
+})
+
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/users/register', (req, res) => {
+  res.send('register')
+})
+
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
 })
 
 app.listen(PORT, () => {
