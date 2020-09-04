ab914b017634f646c6c56252e11b229867469f68
Corey <simon8888xsimon8888x@gmail.com>
2020-09-04 21:39:15 +0800

    refactor: routes/users

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const bodyParser = require('body-parser')
 const session = require('express-session')
 const usePassport = require('./config/passport')
 const passport = require('passport')
+const router = require('./routes/index')
 
 const app = express()
 app.engine('hbs', exphbs({ defaultLayout: "main", extname: "hbs" }))
@@ -47,46 +48,46 @@ app.get('/', (req, res) => {
     .catch((error) => { return res.status(422).json(error) })
 })
 
-app.get('/users/login', (req, res) => {
-  res.render('login')
-})
+// app.get('/users/login', (req, res) => {
+//   res.render('login')
+// })
 
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
+// app.post('/users/login', (req, res) => {
+//   res.send('login')
+// })
 
-app.get('/users/register', (req, res) => {
-  res.render('register')
-})
+// app.get('/users/register', (req, res) => {
+//   res.render('register')
+// })
 
-app.post('/users/register', (req, res) => {
-  const { name, email, password, confirmPassword } = req.body
-  User.findOne({ where: { email } }).then(user => {
-    if (user) {
-      console.log('User already exists')
-      return res.render('register', {
-        name,
-        email,
-        password,
-        confirmPassword
-      })
-    }
-    return bcrypt
-      .genSalt(10)
-      .then(salt => bcrypt.hash(password, salt))
-      .then(hash => User.create({
-        name,
-        email,
-        password: hash
-      }))
-      .then(() => res.redirect('/'))
-      .catch(err => console.log(err))
-  })
-})
+// app.post('/users/register', (req, res) => {
+//   const { name, email, password, confirmPassword } = req.body
+//   User.findOne({ where: { email } }).then(user => {
+//     if (user) {
+//       console.log('User already exists')
+//       return res.render('register', {
+//         name,
+//         email,
+//         password,
+//         confirmPassword
+//       })
+//     }
+//     return bcrypt
+//       .genSalt(10)
+//       .then(salt => bcrypt.hash(password, salt))
+//       .then(hash => User.create({
+//         name,
+//         email,
+//         password: hash
+//       }))
+//       .then(() => res.redirect('/'))
+//       .catch(err => console.log(err))
+//   })
+// })
 
-app.get('/users/logout', (req, res) => {
-  res.send('logout')
-})
+// app.get('/users/logout', (req, res) => {
+//   res.send('logout')
+// })
 
 app.get('/todos/:id', (req, res) => {
   const id = req.params.id
@@ -94,6 +95,8 @@ app.get('/todos/:id', (req, res) => {
     .then(todo => res.render('detail', { todo: todo.toJSON() }))
     .catch(error => console.log(error))
 })
+
+app.use(router)
 
 app.listen(PORT, () => {
   console.log('server is enable~')
